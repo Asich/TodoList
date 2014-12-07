07cc52881a965a4ca67649859ce470792a5733ec
Asich <amustafin@crystalspring.kz>
2014-12-07 22:27:32 +0600

    Initial Commit

---
+++ TodoList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TodoList
+//
+//  Created by Mustafin Askar on 07/12/2014.
+//  Copyright (c) 2014 askar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
